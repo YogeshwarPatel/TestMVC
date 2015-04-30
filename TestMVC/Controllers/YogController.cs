6d5c1c5d764e9ec87caa0c4231def9401032a250
YogeshwarPatel <Yogeshwar.Patel@Gmail.com>
2015-04-30 22:34:37 +1000

    bootstrap added
    bootstrap added - new jquery version added from nuget

---
+++ TestMVC/Controllers/YogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC.Controllers
+{
+    public class YogController : Controller
+    {
+        //
+        // GET: /Yog/
+
+        public ActionResult Index()
+        {
+            return View("MyProfile");
+        }
+
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+    }
+}
